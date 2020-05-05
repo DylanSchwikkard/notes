065f8163b0995ff3416f00b02a459153607d5b07
Dylan Schwikkard <dwschwikkard@gmail.com>
2020-05-05 14:53:03 +0200

    Fixed colors

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,7 +1,6 @@
 import { Note } from "./../models/note.model";
 import { Injectable } from "@angular/core";
 import { Storage } from "@ionic/storage";
-import { exists } from "fs";
 
 @Injectable({
   providedIn: "root",
